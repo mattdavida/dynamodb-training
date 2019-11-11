4555b2184cfab3450ed4213db314f75a502c4c40
matthew arvidson <mattdavida@outlook.com>
2019-11-11 10:54:31 -0800

    add update and delete table methods

--- table-ops.js
+++ table-ops.js
@@ -3,16 +3,16 @@ AWS.config.update({ region: "us-east-2" });
 
 const dynamodb = new AWS.DynamoDB();
 
-// dynamodb.listTables({}, (err, data) => {
-//     if (err) {
-//         console.error(err);
-//     }
-//     console.log(data)
-// }) 
+dynamodb.listTables({}, (err, data) => {
+    if (err) {
+        console.error(err);
+    }
+    console.log(data)
+})
 
 
 // dynamodb.describeTable({
-//     TableName: 'td_notes_test'
+//     TableName: 'td_nodes_sdk'
 // }, (err, data) => {
 //     if (err) {
 //         console.error(err);
@@ -20,35 +20,57 @@ const dynamodb = new AWS.DynamoDB();
 //     console.log(JSON.stringify(data, null, 2))
 // })
 
-dynamodb.createTable({
-    TableName: 'td_nodes_sdk',
-    AttributeDefinitions: [
-        {
-            AttributeName: "user_id",
-            AttributeType: "S"
-        },
-        {
-            AttributeName: "timestamp",
-            AttributeType: "N"
-        },
-    ],
-    KeySchema: [
-        {
-            AttributeName: "user_id",
-            KeyType: "HASH"
-        },
-        {
-            AttributeName: 'timestamp',
-            KeyType: "RANGE"
-        }
-    ],
-    ProvisionedThroughput: {
-        ReadCapacityUnits: 1,
-        WriteCapacityUnits: 1
-    }
-}, (err, data) => {
-    if (err) {
-        console.error(err);
-    }
-    console.log(JSON.stringify(data, null, 2))
-})
+// dynamodb.createTable({
+//     TableName: 'td_nodes_sdk',
+//     AttributeDefinitions: [
+//         {
+//             AttributeName: "user_id",
+//             AttributeType: "S"
+//         },
+//         {
+//             AttributeName: "timestamp",
+//             AttributeType: "N"
+//         },
+//     ],
+//     KeySchema: [
+//         {
+//             AttributeName: "user_id",
+//             KeyType: "HASH"
+//         },
+//         {
+//             AttributeName: 'timestamp',
+//             KeyType: "RANGE"
+//         }
+//     ],
+//     ProvisionedThroughput: {
+//         ReadCapacityUnits: 1,
+//         WriteCapacityUnits: 1
+//     }
+// }, (err, data) => {
+//     if (err) {
+//         console.error(err);
+//     }
+//     console.log(JSON.stringify(data, null, 2))
+// })
+
+// dynamodb.updateTable({
+//     TableName: "td_nodes_sdk",
+//     ProvisionedThroughput: {
+//         ReadCapacityUnits: 2,
+//         WriteCapacityUnits: 1
+//     }
+// }, (err, data) => {
+//     if (err) {
+//         console.error(err);
+//     }
+//     console.log(JSON.stringify(data, null, 2))
+// })
+
+// dynamodb.deleteTable({
+//     TableName: 'td_nodes_sdk'
+// }, (err, data) => {
+//     if (err) {
+//         console.error(err);
+//     }
+//     console.log(JSON.stringify(data, null, 2))
+// })   
